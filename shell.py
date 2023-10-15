3d5b6a20773c8982493043a48a23047e89bbc185
cyz20040527 <145026392+cyz20040527@users.noreply.github.com>
2023-10-15 16:14:45 +0800

    这是第二次shell源代码的提交

--- shell.py
+++ shell.py
@@ -1,12 +1,14 @@
+#解析命令行输入
 import shlex
 def parse_command(command):
-    # 使用shlex模块解析命令行输入
+    ## 使用shlex模块解析命令行输入
         split_command = shlex.split(command)
         return split_command
 
+#创建子进程和执行命令
 import subprocess
 def execute_command(command):
-    # 使用subprocess执行命令
+    ## 使用subprocess执行命令
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output, error = process.communicate()
     if error:
@@ -14,23 +16,232 @@ def execute_command(command):
     else:
         print(output.decode())
 
+#记录历史命令
 import os
 HISTORY_FILE = os.path.join(os.path.expanduser("~"), ".yourshellname_history")
 def save_history(command):
     with open(HISTORY_FILE, "a") as f:
         f.write(command + "\n")
 
+
+#实现基本的shell内建命令
 import os
 def exit_shell():
     # 退出shell
     os._exit(0)
 
+#实现.、...、~和-
 import os
 def expand_path(path):
-    # 解析路径
+    ## 解析路径
     if path == "~":
         return os.path.expanduser("~")
-    elif path == ".-":  # 当前目录和上一级目录的快捷方式
+    elif path == ".-":  ## 当前目录和上一级目录的快捷方式
         return os.path.join(os.getcwd(), "../")
     else:
         return path
+
+#实现错误提示信息
+import os
+import logging
+import sys
+import select
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+def execute_command(command):
+    try:
+        output = os.popen(command).read()
+        logging.debug(f"Command '{command}' executed with output: {output}")
+        print(output)
+    except Exception as e:
+        logging.error(f"Error occurred while executing command '{command}': {e}")
+        print(f"\033[91mError occurred while executing command: {e}\033[0m")
+def shell():
+    while True:
+        try:
+            command = input(f"\033[曹赟周的Shell\033[0m > ")
+            if command.lower() == 'exit':
+                break
+            execute_command(command)
+        except KeyboardInterrupt:
+            print("\n\033[91mOops! You pressed Ctrl+C! Type 'exit' to quit.\033[0m")
+        except Exception as e:
+            print(f"\033[91mAn error occurred: {e}\033[0m")
+if __name__ == "__main__":
+    shell()
+
+#实现常见的信号处理、管道符、输入输出重定向
+
+##信号处理
+
+import subprocess
+import signal
+### 创建一个子进程
+p = subprocess.Popen(['python', '-c', 'import time; time.sleep(10)'], sigint_signal=signal.SIG_IGN)
+### 10秒后，发送SIGINT信号给子进程
+import time
+time.sleep(10)
+p.send_signal(signal.SIGINT)
+### 等待子进程退出
+p.wait()
+
+##管道符
+import subprocess
+### 前一个命令的输出作为后一个命令的输入
+p1 = subprocess.Popen(['echo', 'hello'], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(['grep', 'l'], stdin=p1.stdout, stdout=subprocess.PIPE)
+### 等待p2完成，并获取结果
+output, _ = p2.communicate()
+print(output.decode())  ### 打印 "hello"
+
+##输入输出重定向
+import subprocess
+with open('output.txt', 'w') as f:
+    process = subprocess.Popen(['ls', '-l'], stdout=f)
+    process.wait()
+
+#实现tab补全功能和上下方向键查找历史命令的功能
+import readline
+import os
+class Completer(object):
+    def __init__(self):
+        self.matches = []
+    def complete(self, text):
+        matches = []
+        # complete only files (not directories)
+        for file in os.listdir('.'):
+            if os.path.isfile(file):
+                if file.startswith(text):
+                    matches.append(file)
+        matches.sort()
+        self.matches = matches
+        return matches[0] if matches else None
+def shell():
+    completer = Completer()
+    readline.parse_and_bind("tab: complete")
+    history = []
+    while True:
+        try:
+            input_data = input(">> ")
+            if input_data == "exit":
+                break
+            result = os.system(input_data)
+            if result != 0:
+                print("Command not recognized")
+            history.append(input_data)
+        except:
+            print("Invalid command, try again.")
+    readline.set_completer(None)
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(completer.complete)
+    readline.parse_and_bind("tab: complete")
+if __name__ == "__main__":
+    shell()
+
+#实现日志系统和环境变量管理
+##set命令可以设置环境变量，例如set key value；
+##get命令可以获取环境变量的值，例如get key；
+##log命令可以设置日志级别，例如log debug；
+##echo命令可以将后面的字符串打印到日志中；
+##其他命令将被视为无效命令。
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+class Shell:
+    def __init__(self):
+        self.logger = logging.getLogger('shell')
+        self.env = {}
+    def set_env(self, key, value):
+        self.env[key] = value
+    def get_env(self, key):
+        return self.env.get(key, None)
+    def execute_command(self, command):
+        parts = command.split()
+        if parts[0] == 'echo':
+            self.logger.info(' '.join(parts[1:]))
+        elif parts[0] == 'set':
+            if len(parts) < 2:
+                print('Usage: set <key> <value>')
+            else:
+                self.set_env(parts[1], ' '.join(parts[2:]))
+                print(f'Set env variable {parts[1]} to {self.get_env(parts[1])}')
+        elif parts[0] == 'get':
+            if len(parts) < 2:
+                print('Usage: get <key>')
+            else:
+                value = self.get_env(parts[1])
+                if value is None:
+                    print(f'Env variable {parts[1]} not found')
+                else:
+                    print(f'Env variable {parts[1]}={value}')
+        elif parts[0] == 'log':
+            if len(parts) < 2:
+                print('Usage: log <level>')
+            else:
+                level = parts[1]
+                if level == 'debug':
+                    self.logger.setLevel(logging.DEBUG)
+                elif level == 'info':
+                    self.logger.setLevel(logging.INFO)
+                elif level == 'warning':
+                    self.logger.setLevel(logging.WARNING)
+                elif level == 'error':
+                    self.logger.setLevel(logging.ERROR)
+                elif level == 'critical':
+                    self.logger.setLevel(logging.CRITICAL)
+                else:
+                    print('Invalid log level')
+        else:
+            print('Command not recognized')
+def main():
+    shell = Shell()
+    file_handler = RotatingFileHandler('shell.log', maxBytes=100000, backupCount=5)
+    file_handler.setLevel(logging.DEBUG)
+    shell.logger.addHandler(file_handler)
+    while True:
+        command = input(">> ")
+        shell.execute_command(command)
+if __name__ == "__main__":
+    main()
+
+#解析shell配置文件
+##读取和解析配置文件
+##创建和初始化shell环境
+##在shell环境中执行命令
+
+###首先需要一个shell配置文件，例如shell.shrc，内容如下：
+#### Shell Configuration
+editor = "vim"
+prompt="lambda: username@hostname"
+history_file = "~/.shell_history"
+
+###以下是一个Python脚本，用于解析这个配置文件并设置相应的环境变量：
+import os
+import re
+class ShellConfig:
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.config = {}
+        self._parse_config()
+    def _parse_config(self):
+        with open(self.config_file, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                key, value = line.split('=', 1)
+                self.config[key.strip()] = value.strip()
+    def get(self, key, default=None):
+        return self.config.get(key, default)
+def main():
+    config = ShellConfig('shell.shrc')
+    os.environ['EDITOR'] = config.get('editor')
+    os.environ['PS1'] = config.get('prompt', '\\u@\\h \\$ ')
+    os.environ['HISTFILE'] = config.get('history_file')
+if __name__ == "__main__":
+    main()
+
+
+#上述代码首先定义了一个ShellConfig类，用于读取和解析shell配置文件。
+# 然后在main函数中创建ShellConfig实例，并从配置文件中获取相关配置，然后设置环境变量。
+# 这里的环境变量设置是永久性的，如果你需要在当前的Python脚本执行结束后还原这些环境变量，需要在设置环境变量前先保存原始的环境变量，然后在脚本结束时恢复它们。
+#这个没有实际执行配置文件中的命令或设置实际的shell环境。要做到这一点，需要一个更复杂的脚本来实现。
